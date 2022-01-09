0c20dd6de649d37f32c3eeb4e85ddf2a1c6168c9
unknown <23sohamsh@gmail.com>
2022-01-09 22:35:31 +0530

    added cors

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const { request, response } = require('express')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
-app.use(express.json())
+app.use(cors()) //middleware for cors policy
+app.use(express.json()) //middle ware for json data
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
-app.use(morgan(':method :url :status :response-time ms :body'));
+app.use(morgan(':method :url :status :response-time ms :body')); //morgan middleware for logging 
 
 let contacts = [
     { 
